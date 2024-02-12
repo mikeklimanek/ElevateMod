731e31fe6c36ef190a2e47ccad6916749ff69fee
mikeklimanek <mike.klimanek@gmail.com>
2024-02-12 01:26:49 +0100

    initializing C# and .NET to start a project

---
+++ elevatemod/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace elevatemod {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("does this work?");
+        }
+    }
+}
+
