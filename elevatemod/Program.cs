92552f439fdbc19117f993618cf7018fe15a9945
mikeklimanek <mike.klimanek@gmail.com>
2024-02-12 13:55:54 +0100

    first exe release, testing daily task

--- elevatemod/Program.cs
+++ elevatemod/Program.cs
@@ -1,10 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace elevatemod {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("does this work?");
+namespace elevatemod 
+{
+    class Program 
+    {
+        static async Task Main(string[] args) 
+        {
+            Console.WriteLine("Daily task started");
+            var targetTime = new TimeSpan(13, 40, 00);
+
+            while(true)
+            {
+                var currentTime = DateTime.Now.TimeOfDay;
+                var timeToWait = targetTime - currentTime;
+
+                if (timeToWait < TimeSpan.Zero)
+                {
+                    timeToWait += TimeSpan.FromDays(1);
+                }
+
+                Console.WriteLine($"Waiting for {timeToWait} to perform daily task.");
+
+                await Task.Delay(timeToWait);
+
+                PerformDailyTask();
+            }
+
+        }
+
+        static void PerformDailyTask()
+        {
+            Console.WriteLine($"Daily task performed at {DateTime.Now}");
         }
     }
 }
